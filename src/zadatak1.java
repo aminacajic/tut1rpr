3d6fb8ba84da43603ee714c6969772d3c157592e
aminacajic <acajic1@etf.unsa.ba>
2023-10-19 14:22:38 +0200

    Initial commit

---
+++ src/zadatak1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class zadatak1 {
+    public static void main(String[] args){
+        int a, b;
+        Scanner unos=new Scanner(System.in);
+        System.out.println("Unesite prvi broj: ");
+        a=unos.nextInt();
+        System.out.println("Unesite drugi broj: ");
+        b=unos.nextInt();
+        System.out.println("Brojevi koje ste unijeli su: " + a + " i " + b);
+    }
+}
